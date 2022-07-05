b1fdf13f1e2515ccb70d1427a68f00e404a86471
haidao0923 <67529758+haidao0923@users.noreply.github.com>
2022-07-05 09:07:59 -0400

    extend support for generating multiple images

--- update_slides.py
+++ update_slides.py
@@ -2,19 +2,17 @@ from pptx import Presentation
 from pptx.util import Inches
 import win32com.client
 import os
-powerpoint_name = '[Name of Powerpoint to Open]'
-image_folder = '[Folder to save images]'
-presentation = Presentation(powerpoint_name)
-slide = presentation.slides[0]
 DATE_INDEX = 2
 TIME_INDEX = 3
 LOCATION_INDEX = 4
-image_name = '[Name of Saved Image]'
-date_text = '[Date of Meeting]'
-location_text = '[Location of Meeting]'
 text_array = []
 
-def get_all_text():
+from PIL import Image
+
+def get_all_text(powerpoint_name, presentation):
+    global text_array
+    text_array = []
+    slide = presentation.slides[0]
     for element in slide.shapes:
         if element.has_text_frame:
             text_array.append(element);
@@ -25,7 +23,7 @@ def modify_text(index, new_text):
     paragraph = text_frame.paragraphs[0]
     paragraph.runs[0].text = new_text
 
-def save():
+def save(powerpoint_name, presentation, image_folder, image_name):
     presentation.save(powerpoint_name)
     powerpoint_path = os.getcwd() + f'/{powerpoint_name}'
     Application = win32com.client.Dispatch('Powerpoint.Application')
@@ -36,7 +34,33 @@ def save():
     Application.Quit()
     os.system('taskkill /F /IM POWERPNT.EXE')
 
-get_all_text()
-modify_text(DATE_INDEX, date_text)
-modify_text(LOCATION_INDEX, location_text)
-save();
+def execute(powerpoint_name, image_folder, image_name, date_text, time_text, location_text):
+    presentation = Presentation(powerpoint_name)
+    get_all_text(powerpoint_name, presentation)
+    for i in range(len(text_array)):
+        print(str(i) + text_array[i].text)
+    modify_text(DATE_INDEX, date_text)
+    modify_text(TIME_INDEX, time_text)
+    modify_text(LOCATION_INDEX, location_text)
+    save(powerpoint_name, presentation, image_folder, image_name);
+
+image_date = '07_10_22'
+text_date = '07/10/22'
+online_image_date = '07_07_22'
+online_text_date = '07/07/22'
+'''
+execute('GGTemplate.pptx', 'Weekly_Images', f'Engage_{image_date}.png',
+        f'Sunday {text_date}', '2pm - 5pm', 'Crosland Tower 2nd Floor')
+execute('GGTemplateSquare.pptx', 'Weekly_Images', f'Instagram_{image_date}.png',
+        f'Sunday {text_date}', '2pm - 5pm', 'Crosland Tower 2nd Floor')
+execute('GGOnlineTemplate.pptx', 'Weekly_Images', f'Engage_Online_{online_image_date}.png',
+        f'Thursday {online_text_date}', '8pm - 10pm', 'Discord Voice Chat - Online')
+execute('GGOnlineTemplateSquare.pptx', 'Weekly_Images', f'Instagram_Online_{online_image_date}.png',
+        f'Thursday {online_text_date}', '8pm - 10pm', 'Discord Voice Chat - Online')
+'''
+first_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_Online_{online_image_date}.png')
+display = Image.new('RGB', (first_image.width * 2 + 10, first_image.height))
+display.paste(first_image, (0, 0))
+second_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_{image_date}.png')
+display.paste(second_image, (first_image.width, 0))
+display.save(os.getcwd() + '/Weekly_Images/' + f'Combined_Instagram_{image_date}.png')
