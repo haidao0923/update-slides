d1de34f000a67d56b0e07813a990e8a73044ac39
Hai Dao <67529758+haidao0923@users.noreply.github.com>
2023-09-19 10:49:28 -0400

    convert input from a string to list for large input

--- update_slides.py
+++ update_slides.py
@@ -44,23 +44,40 @@ def execute(powerpoint_name, image_folder, image_name, date_text, time_text, loc
     modify_text(LOCATION_INDEX, location_text)
     save(powerpoint_name, presentation, image_folder, image_name);
 
-image_date = '07_10_22'
-text_date = '07/10/22'
-online_image_date = '07_07_22'
-online_text_date = '07/07/22'
-'''
-execute('GGTemplate.pptx', 'Weekly_Images', f'Engage_{image_date}.png',
-        f'Sunday {text_date}', '2pm - 5pm', 'Crosland Tower 2nd Floor')
-execute('GGTemplateSquare.pptx', 'Weekly_Images', f'Instagram_{image_date}.png',
-        f'Sunday {text_date}', '2pm - 5pm', 'Crosland Tower 2nd Floor')
-execute('GGOnlineTemplate.pptx', 'Weekly_Images', f'Engage_Online_{online_image_date}.png',
-        f'Thursday {online_text_date}', '8pm - 10pm', 'Discord Voice Chat - Online')
-execute('GGOnlineTemplateSquare.pptx', 'Weekly_Images', f'Instagram_Online_{online_image_date}.png',
-        f'Thursday {online_text_date}', '8pm - 10pm', 'Discord Voice Chat - Online')
-'''
-first_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_Online_{online_image_date}.png')
-display = Image.new('RGB', (first_image.width * 2 + 10, first_image.height))
-display.paste(first_image, (0, 0))
-second_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_{image_date}.png')
-display.paste(second_image, (first_image.width, 0))
-display.save(os.getcwd() + '/Weekly_Images/' + f'Combined_Instagram_{image_date}.png')
+wednesday_date = ["09_27_23"]
+sunday_date = ["10_01_23"]
+wednesday_location_list = ['Student Center Peachtree']
+sunday_location_list = ['Student Center Northside']
+
+for i in range(len(wednesday_date)):
+    image_date = sunday_date[i]
+    text_date = image_date[0:2] + "/" + image_date[3:5] + "/" + image_date[6:8]
+    location = sunday_location_list[i]
+    wednesday_location = wednesday_location_list[i]
+    jennaral_meeting_date = wednesday_date[i]
+    jennaral_text_date = jennaral_meeting_date[0:2] + "/" + jennaral_meeting_date[3:5] + "/" + jennaral_meeting_date[6:8]
+    #online_image_date = '01_04_23'
+    #online_text_date = '01/04/23'
+
+    execute('GGTemplate.pptx', 'Weekly_Images', f'Engage_{image_date}.png',
+            f'Sunday {text_date}', '2pm - 5pm', location)
+    execute('GGTemplateSquare.pptx', 'Weekly_Images', f'Instagram_{image_date}.png',
+            f'Sunday {text_date}', '2pm - 5pm', location)
+    execute('JennaralMeetingTemplate.pptx', 'Weekly_Images', f'Engage_Jennaral_{jennaral_meeting_date}.png',
+            f'Wednesday {jennaral_text_date}', '7:30pm - 10:30pm', wednesday_location)
+    execute('JennaralMeetingTemplateSquare.pptx', 'Weekly_Images', f'Instagram_Jennaral_{jennaral_meeting_date}.png',
+            f'Wednesday {jennaral_text_date}', '7:30pm - 10:30pm', wednesday_location)
+    #execute('GGOnlineTemplate.pptx', 'Weekly_Images', f'Engage_Online_{online_image_date}.png',
+    #        f'Wednesday {online_text_date}', '9pm', 'Discord Voice Chat - Online')
+    #execute('GGOnlineTemplateSquare.pptx', 'Weekly_Images', f'Instagram_Online_{online_image_date}.png',
+    #        f'Wednesday {online_text_date}', '9pm', 'Discord Voice Chat - Online')
+    first_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_Jennaral_{jennaral_meeting_date}.png')
+    display = Image.new('RGB', (first_image.width * 2, first_image.height))
+    display.paste(first_image, (0, 0))
+    second_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Instagram_{image_date}.png')
+    display.paste(second_image, (first_image.width, 0))
+    display.save(os.getcwd() + '/Weekly_Images/' + f'Combined_Instagram_{image_date}.png')
+    combined_image = Image.open(os.getcwd() + '/Weekly_Images/' + f'Combined_Instagram_{image_date}.png')
+    bordered_display = Image.new('RGB', (combined_image.width, combined_image.width))
+    bordered_display.paste(combined_image, (0, int((combined_image.width - combined_image.height) / 2)))
+    bordered_display.save(os.getcwd() + '/Weekly_Images/' + f'Bordered_Instagram_{image_date}.png')
